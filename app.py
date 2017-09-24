1565e869970216d755dd48f8087fa40cb3c2d74b
ginanjar azie <xstepnort@gmail.com>
2017-09-24 22:27:11 +0700

    add endpoint to add user and gel all user

--- app.py
+++ app.py
@@ -1,11 +1,59 @@
-from flask import Flask
-# import flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('username', 'email')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/user", methods=["POST"])
+def add_user():
+    username = request.form['username']
+    email = request.form['email']
+    print(username)
+    print(email)
+    new_user = User(username, email)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user)
+
+
+@app.route("/user", methods=["GET"])
+def get_user():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result.data)
 
 
 if __name__ == '__main__':
